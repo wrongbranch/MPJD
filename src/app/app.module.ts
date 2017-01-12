c1414e572320cf2d5df003da3796ce16f2934d20
on_origami <yonosamochi@me.com>
2017-01-12 16:29:21 +0900

    angular-router module追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+    //TODO: Insert 何か here
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +18,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
